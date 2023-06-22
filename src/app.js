a17554af8ceaa54428043d6bfab3b4f0b5a7b225
yagobelo <yago.belo00@gmail.com>
2023-06-22 20:50:23 -0300

    feat: introduting swagger document

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./database/db");
 const pictureRoutes = require("./routes/pictureRoutes");
+const swaggerRoutes = require("./routes/swaggerRoutes");
+
 require("dotenv").config();
 
 connectDB();
@@ -9,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/picture", pictureRoutes);
+app.use("/doc", swaggerRoutes);
 
 const PORT = process.env.PORT;
 
